f4a06c73be1566893bdf535959fba542ec2e3139
Etukudo Emmanuel <etukzbong@gmail.com>
2023-06-09 20:34:56 +0100

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# image_labelling_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/image_labelling_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.image_labelling_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
