d82a205fda3af67df8cb682728a3a5b7df28704b
Etukudo Emmanuel <etukzbong@gmail.com>
2023-06-11 00:00:24 +0100

    image to text  fearure

--- lib/features/image_to_text/image_to_text.dart
+++ lib/features/image_to_text/image_to_text.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:image_labelling_app/common_widgets/common_fab.dart';
+import 'package:image_labelling_app/common_widgets/select_image_or_camera_button.dart';
 import 'package:image_labelling_app/constants/app_colors.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageToText extends StatefulWidget {
   const ImageToText({super.key});
@@ -10,11 +15,104 @@ class ImageToText extends StatefulWidget {
 }
 
 class _ImageToTextState extends State<ImageToText> {
+  bool textScanning = false;
+
+  XFile? imageFile;
+
+  String scannedText = "";
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: AppColors.yellowish,
       floatingActionButton: CommonFAB(bgColor: AppColors.yellowish),
+      body: Center(
+          child: SingleChildScrollView(
+        child: Container(
+            margin: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (textScanning) const CircularProgressIndicator(),
+                if (!textScanning && imageFile == null)
+                  Container(
+                    width: 300,
+                    height: 300,
+                    color: Colors.grey[300]!,
+                  ),
+                if (imageFile != null) Image.file(File(imageFile!.path)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SelectImageOrCameraButton(
+                        title: 'Gallery',
+                        textColor: AppColors.yellowish,
+                        iconColor: AppColors.yellowish,
+                        onPressed: () {
+                          getImage(ImageSource.gallery);
+                        },
+                        icon: Icons.image),
+                    SelectImageOrCameraButton(
+                      title: 'Camera',
+                      icon: Icons.camera_alt,
+                      iconColor: AppColors.yellowish,
+                      textColor: AppColors.yellowish,
+                      onPressed: () {
+                        getImage(ImageSource.camera);
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: Text(
+                    scannedText,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                )
+              ],
+            )),
+      )),
     );
+  }
+
+  void getImage(ImageSource source) async {
+    try {
+      final pickedImage = await ImagePicker().pickImage(source: source);
+      if (pickedImage != null) {
+        textScanning = true;
+        imageFile = pickedImage;
+        setState(() {});
+        getRecognisedText(pickedImage);
+      }
+    } catch (e) {
+      textScanning = false;
+      imageFile = null;
+      scannedText = "Error occured while scanning";
+      setState(() {});
+    }
+  }
+
+  void getRecognisedText(XFile image) async {
+    final inputImage = InputImage.fromFilePath(image.path);
+    final TextRecognizer textRecognizer =
+        TextRecognizer(script: TextRecognitionScript.latin);
+    final recognisedText = await textRecognizer.processImage(inputImage);
+    await textRecognizer.close();
+    scannedText = "";
+    for (TextBlock block in recognisedText.blocks) {
+      for (TextLine line in block.lines) {
+        scannedText = "$scannedText${line.text}\n";
+      }
+    }
+    textScanning = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
   }
 }
